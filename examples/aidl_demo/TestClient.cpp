bb99a0a84a1dc1606d680ed983a88a06fedd27c5
cuiziwei <cuiziwei@xiaomi.com>
2023-04-11 21:28:10 +0800

    binder: Add aidl_demo
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

---
+++ examples/aidl_demo/TestClient.cpp
@@ -0,0 +1,41 @@
+#define LOG_TAG "TestClient"
+
+#include <binder/IPCThreadState.h>
+#include <binder/ProcessState.h>
+#include <binder/IServiceManager.h>
+
+#include "ITestStuff.h"
+#include "BnTestStuff.h"
+#include "BpTestStuff.h"
+
+#include <utils/Log.h>
+#include <utils/String8.h>
+
+using namespace android;
+using android::binder::Status;
+
+extern "C" int main(int count, char **argv)
+{
+    ALOGI("demo client start count: %d, argv[0]: %s", count, argv[0]);
+
+    // obtain service manager
+    sp<IServiceManager> sm(defaultServiceManager());
+    ALOGI("defaultServiceManager(): %p", sm.get());
+
+    // obtain demo.service
+    sp<IBinder> binder = sm->getService(String16("aidldemo.service"));
+    if (binder == NULL) {
+        ALOGE("aidldemo service binder is null, abort...");
+        return -1;
+    }
+    ALOGI("aidldemo service binder is %p", binder.get());
+
+    // by interface_cast restore ITest
+    sp<ITestStuff> service = interface_cast<ITestStuff>(binder);
+    ALOGI("aidldemo service is %p", service.get());
+
+    // use service provided by demo.service
+    service->write(123);
+    service->read(456);
+    return 0;
+}
---
+++ examples/aidl_demo/TestServer.cpp
@@ -0,0 +1,56 @@
+#define LOG_TAG "TestService"
+
+#include <binder/IPCThreadState.h>
+#include <binder/ProcessState.h>
+#include <binder/IServiceManager.h>
+
+#include "ITestStuff.h"
+#include "BnTestStuff.h"
+#include "BpTestStuff.h"
+
+#include <utils/Log.h>
+#include <utils/String8.h>
+
+using namespace android;
+using android::binder::Status;
+
+namespace android {
+class ITestServer : public BnTestStuff {
+public:
+    // ITestServer::read()
+    Status read(int32_t sample) {
+        ALOGD("ITest::read() called %d, start do hard work", sample);
+        return Status::ok();
+    }
+
+    // ITestServer::write()
+    Status write(int32_t index) {
+        ALOGD("ITest::write() called %d, start do hard work", index);
+        return Status::ok();
+    }
+};
+} // namespace android
+
+extern "C" int main(int argc, char **argv)
+{
+    ALOGI("sample service start count: %d, argv[0]: %s", argc, argv[0]);
+
+    // create ProcessState
+    sp<ProcessState> proc(ProcessState::self());
+
+    // obtain service manager
+    sp<IServiceManager> sm(defaultServiceManager());
+    ALOGI("defaultServiceManager(): %p", sm.get());
+    sp<ITestServer> testServer = new ITestServer;
+
+    // add service
+    sm->addService(String16("aidldemo.service"), testServer);
+    ALOGI("add aidldemo.service to service manager");
+
+    // start worker thread
+    ProcessState::self()->startThreadPool();
+
+    // join the main thread
+    IPCThreadState::self()->joinThreadPool();
+    return 0;
+}
