dde6b71de390e90d0890c86f74ba08b422a89013
huanghanqing <huanghanqing@xiaomi.com>
2023-04-23 10:16:27 +0800

    fix aidl_demo compile warning
    Summary:
    
      TestClientCpc.cpp compile warning [-Wformat=]
      FIX: TestClientCpc.cpp:31:15: warning: format ‘%p’ expects argument of type ‘void*’, but argument 4 has type ‘android::sp<ITestStuff>’ [-Wformat=]
       31 |         ALOGI("service:%p", service);
    
    Note:
    	NULL
    
    Signed-off-by: huanghanqing <huanghanqing@xiaomi.com>

--- examples/aidl_demo/TestClientCpc.cpp
+++ examples/aidl_demo/TestClientCpc.cpp
@@ -3,21 +3,21 @@
 #include <binder/IPCThreadState.h>
 #include <binder/ProcessState.h>
 
-#include "ITestStuff.h"
 #include "BnTestStuff.h"
 #include "BpTestStuff.h"
+#include "ITestStuff.h"
 
-#include <utils/Log.h>
-#include <utils/String8.h>
+#include <binder/RpcCertificateFormat.h>
 #include <binder/RpcServer.h>
 #include <binder/RpcSession.h>
-#include <binder/RpcCertificateFormat.h>
 #include <binder/RpcTransportRaw.h>
+#include <utils/Log.h>
+#include <utils/String8.h>
 
 using namespace android;
 using android::binder::Status;
 
-extern "C" int main(int count, char **argv)
+extern "C" int main(int count, char** argv)
 {
     ALOGI("client RpcSession::make...");
     auto session = RpcSession::make();
@@ -28,7 +28,6 @@ extern "C" int main(int count, char **argv)
     // interface_cast restore ITest interface
     sp<ITestStuff> service = interface_cast<ITestStuff>(remoteBinder);
     if (service) {
-        ALOGI("service:%p", service);
         // callback server service interface
         service->write(123);
         service->read(456);
