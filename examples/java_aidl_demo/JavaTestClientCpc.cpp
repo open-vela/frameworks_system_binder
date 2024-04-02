3441362e1ca80308bd2ce3a6aac27871486d956f
zhangbo56 <zhangbo56@xiaomi.com>
2024-04-02 16:01:21 +0800

    binder: Add android java related test code
    Both for android java and vela cpp test for cpcbinder.
    
    java app should be pushed to android device/emulator, a cpctest
    file helper will be provided for app running.
    
    Signed-off-by: zhangbo56 <zhangbo56@xiaomi.com>

---
+++ examples/java_aidl_demo/JavaTestClientCpc.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2024 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "JavaTestClient"
+
+#include "src/IJavaTestStuff.h"
+
+#include <binder/ICpcServiceManager.h>
+#include <utils/Log.h>
+
+using namespace android;
+using android::binder::Status;
+
+extern "C" int main(int argc, char** argv)
+{
+    sp<IServiceManager> sm(defaultCpcServiceManager());
+
+    auto remoteBinder = sm->getService(String16("asvc_cpctest"));
+
+    if (!remoteBinder) {
+        ALOGI("remoteBinder get nullptr error");
+        return 0;
+    }
+    // interface_cast restore ITest interface
+    sp<src::IJavaTestStuff> service = interface_cast<src::IJavaTestStuff>(remoteBinder);
+    if (service) {
+        // callback server service interface
+        service->write(123);
+        service->read(456);
+    } else {
+        ALOGI("remoteBinder get is null error\n");
+    }
+    return 0;
+}
