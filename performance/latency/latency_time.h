da2af6e604bc80d9a2a2a4ebb3c05ae3b62b684d
cuiziwei <cuiziwei@xiaomi.com>
2024-11-19 18:29:33 +0800

    binderlib: Remove redundant comments and rename the incoming parameters.
    Change-Id: I4a3c3a1c6d76a6c5203f6189528872db137ef46e

--- performance/latency/latency_time.h
+++ performance/latency/latency_time.h
@@ -23,53 +23,52 @@
 #define deadline_us 2500
 #define GOOD_SYNC_MIN 0.6
 
-typedef std::chrono::time_point<std::chrono::high_resolution_clock> Tick;
+typedef std::chrono::time_point<std::chrono::high_resolution_clock> Ticks;
 
-static inline Tick tickNow()
+static inline Ticks tickNow()
 {
     return std::chrono::high_resolution_clock::now();
 }
 
-static inline uint64_t tickNano(Tick& sta, Tick& end)
+static inline uint64_t tickNano(Ticks& sta, Ticks& end)
 {
     return uint64_t(std::chrono::duration_cast<std::chrono::nanoseconds>(end - sta).count());
 }
 
 struct Results {
-    uint64_t m_best = 0xffffffffffffffffULL;
-    uint64_t m_worst = 0;
-    uint64_t m_transactions = 0;
-    uint64_t m_total_time = 0;
+    uint64_t best = 0xffffffffffffffffULL;
+    uint64_t worst = 0;
+    uint64_t trans = 0;
+    uint64_t total_time = 0;
     uint64_t m_miss = 0;
     bool tracing;
     explicit Results(bool _tracing)
         : tracing(_tracing)
     {
     }
-    inline bool miss_deadline(uint64_t nano)
+    inline bool miss_deadline(uint64_t nanos)
     {
-        return nano > deadline_us * 1000;
+        return nanos > deadline_us * 1000;
     }
-    void add_time(uint64_t nano)
+    void add_time(uint64_t nanos)
     {
-        m_best = std::min(nano, m_best);
-        m_worst = std::max(nano, m_worst);
-        m_transactions += 1;
-        m_total_time += nano;
-        if (miss_deadline(nano))
+        best = std::min(nanos, best);
+        worst = std::max(nanos, worst);
+        trans += 1;
+        total_time += nanos;
+        if (miss_deadline(nanos))
             m_miss++;
     }
     void dump()
     {
-        double best = (double)m_best / 1.0E6;
-        double worst = (double)m_worst / 1.0E6;
-        double average = (double)m_total_time / m_transactions / 1.0E6;
-        // TODO: libjson?
+        double best = (double)best / 1.0E6;
+        double worst = (double)worst / 1.0E6;
+        double average = (double)total_time / trans / 1.0E6;
         int W = DUMP_PRESICION + 2;
 
         printf("{ \"avg\":%*.*f,\"wst\":%*.*f,\"bst\":%*.*f,\"miss\":%" PRIu64 ",\"meetR\":%.3f}\n",
             W, DUMP_PRESICION, average, W, DUMP_PRESICION, worst, W, DUMP_PRESICION,
-            best, m_miss, 1.0 - (double)m_miss / m_transactions);
+            best, m_miss, 1.0 - (double)m_miss / trans);
     }
 };
 
